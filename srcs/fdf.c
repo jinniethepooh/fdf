2bd5ad84442e1966c18dd494b61b76d3fe110f45
jinniethepoohc <jinnie@jinnie-mbp.local>
2022-10-09 13:05:30 +0700

    [revised] continuously render for key-press

--- srcs/fdf.c
+++ srcs/fdf.c
@@ -6,7 +6,7 @@
 /*   By: cchetana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 03:20:33 by cchetana          #+#    #+#             */
-/*   Updated: 2022/06/16 03:20:46 by cchetana         ###   ########.fr       */
+/*   Updated: 2022/10/09 13:04:34 by cchetana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	main(int argc, char **argv)
 	map_render(&info);
 	mlx_mouse_hook(info.mlx_win, mouse_press, &info);
 	mlx_hook(info.mlx_win, EXIT_CLICK, 0, close_window, &info);
-	mlx_key_hook(info.mlx_win, key_press, &info);
+	mlx_hook(info.mlx_win, 2, 1L << 0, key_press, &info);
 	mlx_loop(info.mlx);
 	close_window(&info);
 	return (0);
